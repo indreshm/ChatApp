0d3a5fb08d389cf0a05f68a60ddd0c401401a11a
Indresh Maurya <indreshmauryakvs@gmail.com>
2024-11-27 00:43:16 +0530

    Final App Complete

--- Backend/Frontend/src/context/SocketContext.jsx
+++ Backend/Frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5002/", {
+      const socket = io("https://chatapp-2fnr.onrender.com", {
         query: {
           userId: authUser.user._id,
         },
